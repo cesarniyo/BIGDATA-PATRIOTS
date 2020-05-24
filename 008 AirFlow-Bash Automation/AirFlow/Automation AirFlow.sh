0a562903a72b58a68ecade73dbd872fe1011daf2
cesarniyo <niyombofils@gmail.com>
2020-05-23 20:18:57 -0400

    updated

---
+++ 008 AirFlow-Bash Automation/AirFlow/Automation AirFlow.sh
@@ -0,0 +1,3 @@
+#https://airflow.apache.org/docs/stable/start.html
+#https://www.youtube.com/watch?v=43wHwwZhJMo&t=126s
+#http://michal.karzynski.pl/blog/2017/03/19/developing-workflows-with-apache-airflow/
